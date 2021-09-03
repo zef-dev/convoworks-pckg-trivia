9efad161efd390932c8c7735775760d02a834a88
mmarcec007 <mark.marcec@gmail.com>
2021-09-03 15:20:07 +0200

    Update evaluateString method due to new APL Feature

--- src/Convo/Pckg/Trivia/TriviaScoresReader.php
+++ src/Convo/Pckg/Trivia/TriviaScoresReader.php
@@ -111,9 +111,9 @@ class TriviaScoresReader extends \Convo\Core\Workflow\AbstractWorkflowContainerC
         }
     }
 
-    public function evaluateString( $string, $context=[]) {
+    public function evaluateString( $string, $context=[], $useHashtagSign = false) {
         $own_params	= $this->getService()->getAllComponentParams( $this);
-        return parent::evaluateString( $string, array_merge( $own_params, $context));
+        return parent::evaluateString( $string, array_merge( $own_params, $context), $useHashtagSign);
     }
 
     private function _getUsers()
